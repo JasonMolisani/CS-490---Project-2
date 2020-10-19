a237d3c067d13a7f3fa2a576d433eb4a0af7d310
Jason Molisani <jm979@njit.edu>
2020-10-19 15:05:08 +0000

    Updated Message history to properly display the profile pic as a form of sender identification. Also added conditional html to not display message history before login

--- scripts/MessageHistory.jsx
+++ scripts/MessageHistory.jsx
@@ -3,24 +3,37 @@ import { Socket } from './Socket';
 
 export function MessageHistory() {
     const [messages, setMessages] = React.useState([]);
+    const [loggedIn, setLoginState] = React.useState(false)
     
-    function getMessages() {
+    function setup() {
         React.useEffect(() => {
             Socket.on('Chat history received', (data) => {
                 console.log("Received Chat history from server: " + data['chatHistory']);
                 setMessages(data['chatHistory']);
             })
         });
+        
+        React.useEffect(() => {
+            Socket.on('login accepted', (data) => {
+                setLoginState(true);
+                console.log("MessageHistory saw server acknowledging login")
+            })
+        });
     }
     
-    getMessages();
+    setup();
     
     return (
         <div id="messageHistory">
-            <ul>
-                {messages.map((message, index) =>
-                    <li key={index} class={message['class']}>{message['sender']}: {message['message']}</li>)}
-            </ul>
+            {loggedIn ?
+                <ul>
+                    {messages.map((message, index) =>
+                        <li key={index} className={message['class']}><img src={message['senderPic']} className='profileImage' />: {message['message']}</li>)}
+                </ul>
+            :
+                <div id="obscuredMessageHistory">
+                </div>
+            }
         </div>
     )
 }
