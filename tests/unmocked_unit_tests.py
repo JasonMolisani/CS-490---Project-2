a3e4b1209465ed4bdc96fd3e2a42e531bbec3a26
Jason Molisani <jm979@njit.edu>
2020-10-24 06:32:10 +0000

    Set up files to track and implement unit testing. Files are currently empty templates that test nothing

---
+++ tests/unmocked_unit_tests.py
@@ -0,0 +1,39 @@
+import unittest
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+class SplitTestCase(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "",
+                KEY_EXPECTED: ""
+            }
+        ]
+        
+        self.failure_test_params = [
+            {
+                KEY_INPUT: "",
+                KEY_EXPECTED: "a"
+            }
+        ]
+
+
+    def test_success(self):
+        for test in self.success_test_params:
+            response = test[KEY_INPUT]
+            expected = test[KEY_EXPECTED]
+            
+            self.assertEqual(response, expected)
+            
+    def test_failure(self):
+        for test in self.failure_test_params:
+            response = test[KEY_INPUT]
+            expected = test[KEY_EXPECTED]
+            
+            self.assertNotEqual(response, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
